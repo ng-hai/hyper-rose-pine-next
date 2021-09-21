ac11663017c953912262c6cbb0dfdac084a3e31a
ng-hai <git@ng-hai.dev>
2021-09-22 01:51:20 +0700

    Inline palette

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
-import RosePine from "@rose-pine/palette"
-
 const PaletteNames = {
   Dark: "dark",
   Moon: "moon",
   Dawn: "dawn",
 }
 
+// https://github.com/rose-pine/palette/blob/main/source/index.ts
 const PaletteColors = {
-  [PaletteNames.Dark]: RosePine.dark,
-  [PaletteNames.Dawn]: RosePine.dawn,
-  [PaletteNames.Moon]: RosePine.moon,
+  [PaletteNames.Dark]: {
+    base: '#191724',
+    surface: '#1f1d2e',
+    overlay: '#26233a',
+    inactive: '#555169',
+    subtle: '#6e6a86',
+    text: '#e0def4',
+    love: '#eb6f92',
+    gold: '#f6c177',
+    rose: '#ebbcba',
+    pine: '#31748f',
+    foam: '#9ccfd8',
+    iris: '#c4a7e7',
+    highlight: '#2a2837',
+    highlightInactive: '#211f2d',
+    highlightOverlay: '#3a384a',
+  },
+  [PaletteNames.Moon]: {
+    base: '#232136',
+    surface: '#2a273f',
+    overlay: '#393552',
+    inactive: '#59546d',
+    subtle: '#817c9c',
+    text: '#e0def4',
+    love: '#eb6f92',
+    gold: '#f6c177',
+    rose: '#ea9a97',
+    pine: '#3e8fb0',
+    foam: '#9ccfd8',
+    iris: '#c4a7e7',
+    highlight: '#312f44',
+    highlightInactive: '#2a283d',
+    highlightOverlay: '#3f3c53',
+  },
+  [PaletteNames.Dawn]: {
+    base: '#faf4ed',
+    surface: '#fffaf3',
+    overlay: '#f2e9de',
+    inactive: '#9893a5',
+    subtle: '#6e6a86',
+    text: '#575279',
+    love: '#b4637a',
+    gold: '#ea9d34',
+    rose: '#d7827e',
+    pine: '#286983',
+    foam: '#56949f',
+    iris: '#907aa9',
+    highlight: '#eee9e6',
+    highlightInactive: '#f2ede9',
+    highlightOverlay: '#e4dfde',
+  },
 }
 
 const defaultConfig = {
