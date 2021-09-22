951972c36061687ba4e9e0bbcc11652e7db78469
ng-hai <git@ng-hai.dev>
2021-09-22 15:46:40 +0700

    Update colors

--- themer/dawn.js
+++ themer/dawn.js
@@ -3,7 +3,7 @@ module.exports.colors = {
   // Dawn
   light: {
     accent0: "#b4637a", // Red
-    accent1: "", // Orange
+    accent1: "#ea9d34", // Orange
     accent2: "#ea9d34", // Yellow
     accent3: "#286983", // Green
     accent4: "#d7827e", // Cyan
@@ -11,12 +11,6 @@ module.exports.colors = {
     accent6: "#9893a5", // caret/cursor
     accent7: "#907aa9", // Magenta
     shade0: "#faf4ed", // background color
-    shade1: "", // UI
-    shade2: "#eee9e6", // UI, text selection
-    shade3: "#6e6a86", // UI, code comments
-    shade4: "", // UI
-    shade5: "", // UI
-    shade6: "#575279", // foreground text
     shade7: "#575279", // foreground text
   },
 }
--- themer/moon.js
+++ themer/moon.js
@@ -3,7 +3,7 @@ module.exports.colors = {
   // Moon
   dark: {
     accent0: "#eb6f92", // Red
-    accent1: "", // Orange
+    accent1: "#f6c177", // Orange
     accent2: "#f6c177", // Yellow
     accent3: "#3e8fb0", // Green
     accent4: "#ea9a97", // Cyan
@@ -11,12 +11,6 @@ module.exports.colors = {
     accent6: "#59546d", // caret/cursor
     accent7: "#c4a7e7", // Magenta
     shade0: "#232136", // background color
-    shade1: "", // UI
-    shade2: "#312f44", // UI, text selection
-    shade3: "#817c9c", // UI, code comments
-    shade4: "", // UI
-    shade5: "", // UI
-    shade6: "#e0def4", // foreground text
     shade7: "#e0def4", // foreground text
   },
 }
